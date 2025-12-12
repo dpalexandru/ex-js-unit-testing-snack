4b4608382a7440a2e1a9d0ce9515d4512716a376
dpalexandru <alexpoian91@gmail.com>
2025-12-12 11:33:43 +0100

    [Snack2] test funzione createSlug

--- snaks.js
+++ snaks.js
@@ -1,8 +1,15 @@
+// snack1
 function getInitials(str) {
   const [nome, cognome] = str.split(" ").filter(str => str !== '');
   const inizialeNome = nome.split('')[0].toUpperCase()
   const inizialeCognome = cognome.split('')[0].toUpperCase()
   return `${inizialeNome}.${inizialeCognome}.`;
+}
+
+// snack2
+function createSlug(str) {
+  const strMinuscola = str.toLowerCase();
+  return strMinuscola;
 }
 
 
@@ -11,8 +18,7 @@ function getInitials(str) {
 
 
 
-
-
 module.exports = {
-  getInitials
+  getInitials,
+  createSlug
 }
--- snaks.test.js
+++ snaks.test.js
@@ -1,9 +1,15 @@
-const { getInitials } = require("./snaks.js")
+const { getInitials, createSlug } = require("./snaks.js")
 
 // snack1 
 test('La funzione getInitials restituisce le iniziali di un nome completo. ', () => {
   expect(getInitials("ALex Dessanai")).toBe("A.D.");
   expect(getInitials("aLex rossi")).toBe("A.R.");
   expect(getInitials("aLex  rossi")).toBe("A.R.");
+})
+
+
+// snack2
+test('La funzione createSlug restituisce una stringa in lowercase. ', () => {
+  expect(createSlug("ALex Dessanai")).toBe("alex dessanai");
 
 })
